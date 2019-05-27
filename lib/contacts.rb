c049882cedd6a2b181fb8cead0f1883185981ece
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-27 21:51:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,8 +1,6 @@
 require 'pry'
-
-  # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
+contacts = {
+  "Jon Snow" => {
   #     name: "Jon",
   #     email: "jon_snow@thewall.we", 
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
